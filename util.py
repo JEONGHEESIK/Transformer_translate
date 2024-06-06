1242ef6805f567346e874ec5c6161e426fbe00a3
HEESIK <127959005+JEONGHEESIK@users.noreply.github.com>
2024-06-06 22:56:15 +0900

    Update util.py

--- util.py
+++ util.py
@@ -15,8 +15,8 @@ class ExcelTranslationDataset(Dataset):
 
     def __getitem__(self, idx):
         row = self.data.iloc[idx]
-        src = row[0]
-        trg = row[1]
+        src = str(row['원문'])  # '원문' 열의 데이터를 문자열로 변환
+        trg = str(row['번역문'])  # '번역문' 열의 데이터를 문자열로 변환
 
         src_token_ids = self.src_tokenizer.encode(src, add_special_tokens=True, max_length=self.src_max_length, truncation=True, padding='max_length')
         trg_token_ids = self.trg_tokenizer.encode(trg, add_special_tokens=True, max_length=self.trg_max_length, truncation=True, padding='max_length')
